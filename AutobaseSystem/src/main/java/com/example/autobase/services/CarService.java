e3340faa396e0e03ebf4572ac2d4e0fa5936bac0
Kenchin33 <169151335+Kenchin33@users.noreply.github.com>
2025-03-31 18:18:05 +0300

    Додав CarService для використання окремих сервісів

---
+++ AutobaseSystem/src/main/java/com/example/autobase/services/CarService.java
@@ -0,0 +1,35 @@
+package com.example.autobase.services;
+
+import com.example.autobase.model.Car;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CarService {
+
+    private List<Car> cars = new ArrayList<>();
+    private CarAddService carAddService = new CarAddService();
+    private CarSearchService carSearchService = new CarSearchService();
+    private CarUpdateService carUpdateService = new CarUpdateService();
+    private CarDeleteService carDeleteService = new CarDeleteService();
+
+    // Додавання автомобіля
+    public void addCar(Car car) {
+        carAddService.addCar(cars, car);
+    }
+
+    // Пошук автомобіля за ID
+    public Car findCarById(String carId) {
+        return carSearchService.findCarById(cars, carId);
+    }
+
+    // Оновлення автомобіля
+    public void updateCar(String carId, Car updatedCar) {
+        carUpdateService.updateCar(cars, carId, updatedCar);
+    }
+
+    // Видалення автомобіля
+    public void deleteCar(String carId) {
+        carDeleteService.deleteCar(cars, carId);
+    }
+}
